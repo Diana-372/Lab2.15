1f393ac154dad0e33860ad3dfaa6091a2bae424d
Diana-372 <pinaevadiana372@gmail.com>
2023-12-17 21:51:29 +0300

    Create primer11.py

---
+++ primer11.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#Приведенный выше код создаст каталог new в текущей рабочей папке.
+
+if __name__ == "__main__":
+    import os
+
+    # creating a new directory with the name new
+    os.mkdir("new")
